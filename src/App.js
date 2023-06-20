29c658552846891a4e6cf9c0bc336d5ddf5ac6a0
DiegoAlejandroRJ <diego6382@gmail.com>
2023-06-20 18:53:27 -0500

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Welcome to Siscore Bitches!!
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Buena los Julis!
-        </p>
         <p>
           que quiere socio!?
         </p>
+        <Button/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,8 +21,16 @@ function App() {
         >
           Learn React
         </a>
+
+        
+
       </header>
     </div>
+  );
+}
+function Button() {
+  return (
+    <button>Mirá que chimba de botón!</button>
   );
 }
 
